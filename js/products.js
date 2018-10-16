60fbdcfb11067721f2c1f2a1f896dfe76e55c0ad
Gavin Chown <gavchown@wl.k12.ia.us>
2018-10-16 14:25:20 -0500

    Added products and filters

---
+++ js/products.js
@@ -0,0 +1,242 @@
+var products = [
+
+    {
+        image: "lebrongoldjersey.jpg",
+        title: "Lebron James Jersey",
+        price: "99.99",
+        size: ["S","M","L","XL","XXL"],
+        description: "This Lebron James Jersey has the iconic 23 on the back",
+        colors: ["White","Purple","Gold"],
+        materials: ["70% Cotton","30% Polyester"],
+        brand: "Adidas",
+        categories: ["Men", "Jersey"],
+        product_id: "C1982"
+
+    },  {
+        image: "yellowtshirt.jpg",
+        title: "LBJ Jersey T-shirt",
+        price: "29.99",
+        size: ["S","M","L","XL","XXL"],
+        description: "This Lebron James T-shirt is just like the jersey but has sleeves",
+        colors: ["White","Purple","Gold"],
+        materials: ["100% Cotton"],
+        brand: "Nike",
+        categories: ["Men","T-shirt"],
+        product_id: "C1983"
+
+    },  {
+        image: "white tshirt.jpg",
+        title: "Lakers Crown T-shirt",
+        price: "39.99",
+        size: ["S","M","L","XL","XXL"],
+        description: "This is a gold t-shirt with the crown of King James on it",
+        colors: ["White","Purple","Gold"],
+        materials: ["100% Cotton"],
+        brand: "Nike",
+        categories: ["Men","T-shirt"],
+        product_id: "C1984"
+
+    },  {
+        image: "All-star LBJ Jersey.jpg",
+        title: "All-Star LBJ Jersey",
+        price: "109.99",
+        size: ["S","M","L","XL","XXL"],
+        description: "This is Lebron James' allstar replica jersey",
+        colors: ["Grey","White"],
+        materials: ["70% Cotton","30% Polyester"],
+        brand: "Jordan",
+        categories: ["Men","Jersey"],
+        product_id: "C1985"
+
+    },  {
+        image: "Lebron 15 Equality Shoes.jpg",
+        title: "Lebron 15 Equality Shoes",
+        price: "139.99",
+        size: ["9","10","10.5","11","11.5","12","13","14","15"],
+        description: "This is a special color way of Lebron 15s that symbolize equality in America",
+        colors: ["White","Black"],
+        brand: "Nike",
+        categories: ["Shoes"],
+        product_id: "C1986"
+
+    },  {
+        image:"Solider 10s.jpg",
+        title: "Lebron Soldier 10s",
+        price: "119.99",
+        size: ["9","10","10.5","11","11.5","12","13","14","15"],
+        description: "Lebron's soldier 10 shoe, light grey",
+        colors: ["light grey"],
+        brand: "Nike",
+        categories: ["Shoes"],
+        product_id: "C1987"
+
+    },  {
+        image: "purpleandblackjersey.jpg",
+        title: "LBJ Purple Jersey",
+        price: 99.99,
+        size: ["S","M","L","XL","XXL"],
+        description: "This Lebron James Jersey has the iconic 23 on the back with a purple and black colorway",
+        colors: ["Black","Purple","Gold"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "Nike",
+        categories: ["Men", "Jersey"],
+        product_id: "C1988"
+
+    },  {
+        image: "USmarinejersey.jpg",
+        title: "US Marine Jersey",
+        price: 124.99,
+        size: ["S","M","L","XL","XXL"],
+        description: "Lebron James jersey with a Marine special edition",
+        colors: ["gold","Purple","green"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "Nike",
+        categories: ["Men", "Jersey"],
+        product_id: "C1989"
+
+    },  {
+        image: "Lebronghost15s.jpg",
+        title: "LeBron 15 Ghost",
+        price: 129.99,
+        size: ["9","10","10.5","11","11.5","12","13","14","15"],
+        description: "LeBron 15 shoes with a ghost color way",
+        colors: ["brown"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "Nike",
+        categories: ["Shoes"],
+        product_id: "C1990"
+
+    },  {
+        image: "whitejamestshirt.jpg",
+        title: "White James Tshirt",
+        price: 14.99,
+        size: ["S","M","L","XL","XXL"],
+        description: "Lebron James white tshirt",
+        colors: ["White","Purple","Gold"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "Jordan",
+        categories: ["Men", "T-shirt"],
+        product_id: "C1991"
+
+    },  {
+        image: "lebronshorts.jpg",
+        title: "Lebron James Shorts",
+        price: 24.99,
+        size: ["S","M","L","XL","XXL"],
+        description: "Lebron James numer 23 shorts",
+        colors: ["grey","gold"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "LeBron",
+        categories: ["Men","Shorts"],
+        product_id: "C1992"
+
+    },  {
+        image: "HSjersey.jpg",
+        title: "High School Lebron Jersey",
+        price: 134.99,
+        size: ["S","M","L","XL","XXL"],
+        description: "This Lebron James Jersey has the iconic 23 on the back",
+        colors: ["Green","Gold"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "Adidas",
+        categories: ["Men"],
+        product_id: "C1993"
+
+    },  {
+        image: "lebronorangebox15s.jpg",
+        title: "Lebron 15 Orange Box",
+        price: 124.99,
+        size: ["9","10","10.5","11","11.5","12","13","14","15"],
+        description: "This Lebron James Jersey has the iconic 23 on the back",
+        colors: ["orange"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "Nike",
+        categories: ["Shoes"],
+        product_id: "C1994"
+
+    },  {
+        image: "blacktshirt.jpg",
+        title: "Lebron Black Shirt",
+        price: 14.99,
+        size: ["S","M","L","XL","XXL"],
+        description: "LeBron James black shirt",
+        colors: ["Black"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "Nike",
+        categories: ["Men", "T-shirt"],
+        product_id: "C1995"
+
+    },  {
+        image: "blacksocks.jpg",
+        title: "Lebron James Socks",
+        price: 12.99,
+        size: ["S","M","L","XL","XXL"],
+        description: "Lebron James nike socks",
+        colors: ["Black"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "LeBron",
+        categories: ["Men", "Socks"],
+        product_id: "C1996"
+
+    },  {
+        image: "B&Wsocks.jpg",
+        title: "Lebron James B&W Socks",
+        price: 12.99,
+        size: ["S","M","L","XL","XXL"],
+        description: "Lebron James B&W Nike Socks",
+        colors: ["White","Black"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "LeBron",
+        categories: ["Men","Socks"],
+        product_id: "C1997"
+
+    },  {
+        image: "lebronB&Wsoldier10s.jpg",
+        title: "Lebron Soldier 10 B&W",
+        price: 114.99,
+        size: ["9","10","10.5","11","11.5","12","13","14","15"],
+        description: "LeBron James Soldier 10 series with a black and white colorway",
+        colors: ["White","Black"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "Nike",
+        categories: ["Shoes"],
+        product_id: "C1998"
+
+    },  {
+        image: "greatness.jpg",
+        title: "LBJ Greatness T-shirt",
+        price: 24.99,
+        size: ["S","M","L","XL","XXL"],
+        description: "LBJ Greatness Shirt",
+        colors: ["Blue","Gold","Wine"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "Adidas",
+        categories: ["Men", "T-shirt"],
+        product_id: "C1999"
+
+    },  {
+        image: "soldier9s.jpg",
+        title: "Lebron Soldier 9s",
+        price: 104.99,
+        size: ["9","10","10.5","11","11.5","12","13","14","15"],
+        description: "LBJ Soldier 9s",
+        colors: ["White","Black"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "Nike",
+        categories: ["Shoes"],
+        product_id: "C2000"
+
+    },  {
+        image: "witness.jpg",
+        title: "LBJ Witness T-shirt",
+        price: 14.99,
+        size: ["S","M","L","XL","XXL"],
+        description: "LBJ Witness Greatness T-shirt",
+        colors: ["White"],
+        materials: ["70% Cotton","20% Polyester"],
+        brand: "Nike",
+        categories: ["Men","T-shirt"],
+        product_id: "C2001"
+
+    },
+];
